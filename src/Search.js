e1463267b0e8d38328ff753616091a69633dc5f1
moshe vilner <shootermv@gmail.com>
2020-03-23 14:54:16 +0200

    better formatting for search component

--- src/Search.js
+++ src/Search.js
@@ -1,20 +1,22 @@
 import React from "react";
-import {filterCountries} from './redux/actions';
-import  {useDispatch} from "react-redux";
+import { filterCountries } from './redux/actions';
+import { useDispatch } from "react-redux";
 
 import styled from '@emotion/styled';
 
 const Inpt = styled.input`
- background: transparent;
- outline: none;
- border: none;
- color: turquoise;
+   background: transparent;
+   outline: none;
+   border: none;
+   color: turquoise;
 `;
+
 const Wrap = styled.div`
-  padding: 15px;
-  display: grid;
-  grid-template-columns: 20px 90px;
+    padding: 15px;
+    display: grid;
+    grid-template-columns: 20px 90px;
 `;
+
 const MagIcon = styled.span`
     position:relative;
     display:inline-block;
@@ -35,23 +37,20 @@ const MagIcon = styled.span`
        -moz-transform: rotate(45deg);
         -ms-transform: rotate(45deg);
          -o-transform: rotate(45deg);
-    
-    
 `
 
 
 
 export default function Search() {
-   const dispatch = useDispatch();
-   const search = e => {
-      let term = e.target.value;
-       dispatch(filterCountries(term));
-   };
-   return (
-    <Wrap>
-        <MagIcon/>
-        <Inpt placeholder="search country" onInput={search} />
-       
-    </Wrap>
-   )
+    const dispatch = useDispatch();
+    const search = e => {
+        let term = e.target.value;
+        dispatch(filterCountries(term));
+    };
+    return (
+        <Wrap>
+            <MagIcon />
+            <Inpt placeholder="search country" onInput={search} />
+        </Wrap>
+    )
 }
