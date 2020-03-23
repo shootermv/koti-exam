757a4aa6982c984086e5402f453e768b7aefd7e3
moshe vilner <shootermv@gmail.com>
2020-03-23 12:07:12 +0200

    remove unused code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch} from "react-redux";
 import List from "./List";
 import Details from "./Details";
